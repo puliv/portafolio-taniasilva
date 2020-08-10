a7079d844fd914a9394cdbea85494c4be97bf251
puliv <pverapavez@gmail.com>
2020-08-10 02:24:01 -0400

    ok

--- src/pages/sumateALoVerde.js
+++ src/pages/sumateALoVerde.js
@@ -9,6 +9,20 @@ import foto5 from "../images/verde8.png"
 import foto6 from "../images/verde9.jpg"
 import foto7 from "../images/verde10.jpg"
 import foto8 from "../images/verde11.jpg"
+import foto9 from "../images/verde12.png"
+import foto10 from "../images/verde13.png"
+import foto11 from "../images/verde14.png"
+import foto12 from "../images/verde15.png"
+import foto13 from "../images/verde16.png"
+import foto14 from "../images/verde17.png"
+import foto15 from "../images/verde18.png"
+import foto16 from "../images/verde19.png"
+import foto17 from "../images/verde20.png"
+import foto18 from "../images/verde21.png"
+import foto19 from "../images/verde22.png"
+import foto20 from "../images/verde23.png"
+import foto21 from "../images/verde24.png"
+import foto22 from "../images/verde25.png"
 import user1 from "../images/verde_user1.png"
 import user2 from "../images/verde_user2.png"
 import user3 from "../images/verde_user3.png"
@@ -102,22 +116,112 @@ const SumateALoVerde = () => (
           Durante la tercera etapa, comenzamos a visualizar el producto digital, desde sus colores, texturas, flujos y esquemas, todo aludiendo a la naturaleza y las personas como agentes de cambio. Se utilizó herramientas como el moodboard, sketching y diseño de wireframes.
         </span>
         <div className="siete-images">
-          <img src={foto6} alt="Sumate a lo verde" />
-          <img src={foto7} alt="Sumate a lo verde" />
-          <img src={foto8} alt="Sumate a lo verde" />
-        </div>
-        <div className="bottoms">
           <div>
+            <img src={foto6} alt="Sumate a lo verde" />
             <hr />
             <span>Sketching y diseño de wireframes.</span>
           </div>
           <div>
+            <img src={foto7} alt="Sumate a lo verde" />
+          </div>
+          <div>
+            <img src={foto8} alt="Sumate a lo verde" />
             <hr />
             <span>Moodboard.</span>
           </div>
         </div>
+
       </div>
-      <div></div>
+      <div className="line">
+        <hr />
+        <h3>PROTOTIPO EN BAJA</h3>
+      </div>
+      <div className="ocho">
+        <span>Para el desarrollo de la aplicación, se utilizó el Adobe XD para prototipar los primeros flujos de tarea.</span>
+        <div>
+          <img src={foto9} alt="verde" />
+          <img src={foto10} alt="verde" />
+        </div>
+      </div>
+      <div className="line">
+        <hr />
+        <h3>UX TESTING</h3>
+      </div>
+      <div className="nueve">
+        <div className="nueve-a">
+          <div>
+            <span>En esta última etapa se evaluó el prototipo en baja, desde el diseño y navegación hasta la utilidad y experiencia, utilizando evaluaciones como Test de usabilidad, Mapa de calor, A/B testing y Evaluación heurística. Los resultados en cuanto a contenido y arquitectura de la información fueron positivos, ya que se entendía y quedaban claros, motivaban al usuario a querer utilizar la aplicación. Por otro lado, la visualización de la aplicación no fue satisfactoria, por lo que se rediseñó la gráfica (color, tamaño, ilustración).
+        </span>
+          </div>
+          <div>
+            <img src={foto11} alt="verde" />
+          </div>
+        </div>
+        <div className="nueve-b">
+          <div>
+            <img src={foto12} alt="verde" />
+
+            <hr />
+            <h3>Test de usabilidad</h3>
+
+          </div>
+          <div>
+            <img src={foto13} alt="verde" />
+          </div>
+        </div>
+        <div className="nueve-c">
+          <div>
+            <img src={foto14} alt="verde" />
+            <hr />
+            <h3>Mapa de calor.</h3>
+          </div>
+          <div>
+            <img src={foto15} alt="verde" />
+            <hr />
+            <h3>A/B Testing</h3>
+          </div>
+        </div>
+      </div>
+      <div className="line">
+        <hr />
+        <h3>UX MOCKUP</h3>
+      </div>
+      <div className="diez">
+        <div className="diez-a">
+          <div className="diez-img">
+            <div>
+              <img src={foto16} alt="verde" />
+            </div>
+            <div>
+              <img src={foto17} alt="verde" />
+            </div>
+            <div>
+              <img src={foto18} alt="verde" />
+            </div>
+          </div>
+          <div>
+            <span>
+              El desarrollo de este proyecto fue un gran desafío, por el alto nivel de información sintetizada en una aplicación y los diferentes tipos de usuarios que abordar, desde niños, jóvenes hasta adultos, ya que todos estamos en diferentes niveles de formación y cambiar hábitos es una acción difícil de generar, por la necesidad de conseguir las cosas de manera rápida y fácil. El prototipo final presentado alcanza un buen nivel de testeo, sin embargo, podrían realizarse mejoras si se continuara en el proyecto.
+
+        </span>
+          </div>
+        </div>
+        <div className="diez-b">
+          <div>
+            <img src={foto19} alt="verde" />
+          </div>
+          <div>
+            <img src={foto20} alt="verde" />
+          </div>
+          <div>
+            <img src={foto21} alt="verde" />
+          </div>
+          <div>
+            <img src={foto22} alt="verde" />
+          </div>
+
+        </div>
+      </div>
     </div>
   </div>
 )

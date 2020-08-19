9310cceee42b8e6a7beb1884ff51333f38d8c2d9
puliv <pverapavez@gmail.com>
2020-08-18 20:20:12 -0400

    ok

--- src/components/header.js
+++ src/components/header.js
@@ -1,29 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ScrollTo } from "react-scroll-to";
 import lineMenu from '../images/menu.png'
+import detail from "../images/detail_2.png"
 
-const header = () => {
+
+function Header() {
+  const [navMobile, setNavMobile] = useState(false)
+
+  const openNavMobile = () => {
+    setNavMobile(!navMobile)
+  }
+
   return (
     <ScrollTo>
       {({ scroll }) => (
-        <header className="nav">
-          <div className="nav-left">
-            <h1 onClick={() => scroll({ x: 0, y: 0, smooth: true })}>TS.</h1>
-          </div>
-          <div className="nav-right">
+        <React.Fragment>
+          <header className="nav">
+            <div className="nav-left">
+              <h1 onClick={() => scroll({ x: 0, y: 0, smooth: true })}>TS.</h1>
+            </div>
+            <div className="nav-right">
+              <ul>
+                <li onClick={() => scroll({ x: 0, y: 0, smooth: true })}>INICIO</li>
+                <li onClick={() => scroll({ x: 0, y: 690, smooth: true })}>SOBRE MI</li>
+                <li onClick={() => scroll({ x: 0, y: 2290, smooth: true })}>PROYECTOS</li>
+                <li onClick={() => scroll({ x: 0, y: 3000, smooth: true })}>CONTÁCTAME</li>
+              </ul>
+              <div onClick={openNavMobile}>
+                <img src={lineMenu} alt="menu" />
+              </div>
+            </div>
+          </header>
+          {navMobile && <div className="nav-mobile">
+
             <ul>
               <li onClick={() => scroll({ x: 0, y: 0, smooth: true })}>INICIO</li>
-              <li onClick={() => scroll({ x: 0, y: 630, smooth: true })}>SOBRE MI</li>
-              <li onClick={() => scroll({ x: 0, y: 2180, smooth: true })}>PROYECTOS</li>
-              <li onClick={() => scroll({ x: 0, y: 3000, smooth: true })}>CONTÁCTAME</li>
+              <li>SOBRE MI</li>
+              <li>PROYECTOS</li>
+              <li>CONTÁCTAME</li>
             </ul>
-            <img src={lineMenu} alt="menu" />
-          </div>
-        </header>
+          </div>}
+        </React.Fragment>
       )}
     </ScrollTo>
   )
 }
 
-
-export default header
+export default Header;
